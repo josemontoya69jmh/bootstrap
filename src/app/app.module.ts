0236efb71ec273fccf9d5a268ebc7f9ab3d2552e
josemontoya69jmh <2017josemontoya@gmail.com>
2020-01-14 18:20:00 +0100

    Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
+
+// Lo esencial para empezar a trabajar
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Estas importaciones son importantisimas para el enrutamiento
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
+
+
+// Aqui se van importando los componentes
 import { AppComponent } from './app.component';
 import { Pagina1Component } from './paginas/pagina1/pagina1.component';
 import { Pagina2Component } from './paginas/pagina2/pagina2.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
-import { Routes,RouterModule } from '@angular/router';
 
+// aqui incluiremos las paginas y como las llamaremos
 const routes: Routes = [
   {
     path: 'pagina1',
-    component:Pagina1Component
+    component: Pagina1Component
   },
   {
     path: 'pagina2',
@@ -29,12 +35,14 @@ const routes: Routes = [
 
 
 @NgModule({
+  // En las declaraciones se pone los componentes
   declarations: [
     AppComponent,
     Pagina1Component,
     Pagina2Component,
     EncabezadoComponent
   ],
+  // Aqui lo de las rutas
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -43,7 +51,8 @@ const routes: Routes = [
   providers: [],
   bootstrap: [AppComponent
   ],
-  exports:[RouterModule]
+  // Y tambien se exporta el RouterModule
+  exports: [RouterModule]
 
 
 
